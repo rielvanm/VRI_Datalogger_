5c225009c91c274d2955fb88f0c21a98523b737d
mrielvan <mrielvan@gmail.com>
2025-02-24 18:43:10 +0100

    Research is almost complete..

--- Code/Datalogger/Datalogger.ino
+++ Code/Datalogger/Datalogger.ino
@@ -1,8 +1,23 @@
+extern "C" {
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+#include <stdlib.h>
+}
+
+int freeMemory() {
+    extern int __heap_start, *__brkval;
+    int v;
+    return (int)&v - (__brkval == 0 ? (int)&__heap_start : (int)__brkval);
+}
+
 void setup() {
-    Serial.begin(9600);
+    Serial.begin(115200);
+    Serial.print("Vrij geheugen: ");
+    Serial.print(freeMemory());
+    Serial.println(" bytes");
 }
 
 void loop() {
-    Serial.println("Arduino werkt!");
-    delay(1000);
+    // Je code hier
 }
