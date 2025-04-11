a74c58e2a5f800d825af492292f56085f6b760d5
mrielvan <mrielvan@gmail.com>
2025-04-11 07:30:23 +0200

    Code: Timer starts always on 0

--- Code/Code_logger/LogiTrack_MR1/RTC.cpp
+++ Code/Code_logger/LogiTrack_MR1/RTC.cpp
@@ -1,32 +1,35 @@
-#include "RTC.h"
-#include <RTClib.h>
+#include "RTC.h"                                        /// Own header file
+#include <RTClib.h>                                     /// Adafruit libr DS3231 (RTC_DS3231 class and DateTime)
 
-RTC_DS3231 rtc;
+RTC_DS3231 rtc;                                         /// Communicate DS3231 with I2C
 
-void RTCManager::begin() {
-  if (!rtc.begin()) {
+void RTCManager::begin() {                              /// Object in class RTCManager  
+  if (!rtc.begin()) {                                   /// checks RTC ready on I2C
     Serial.println("RTC not found!");
-    while(1);
+    while(1);                                           /// Endless loop 
   }
-  rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));       /// Systemtime  
 }
 
-void RTCManager::start() {
-  running = true;
-  startTime = rtc.now();
-  startMillis = millis();
+void RTCManager::start() {                              /// Object start   
+  rtc.adjust(DateTime(2000, 1, 1, 0, 0, 0));            /// Reset clock to 0
+  delay(10);                                            /// Delay for stablisation
+  running = true;                                       /// Intern flag stopwatch is running
+  startTime = rtc.now();                                /// This is the start
+  startMillis = millis();                               /// Saves systemtime in ms 
 }
 
-void RTCManager::stopAndReset() {
-  running = false;
-  rtc.adjust(DateTime(2000, 1, 1, 0, 0, 0));
+void RTCManager::stopAndReset() {                       /// Object stopAndReset
+  running = false;                                      /// Intern flag f clock isn't running
+  rtc.adjust(DateTime(2000, 1, 1, 0, 0, 0));            /// Set DS3231 back to 0
+  startTime = DateTime(2000, 1, 1, 0, 0, 0);            /// Local memory startTime to 0
 }
 
-DateTime RTCManager::now() {
-  return running ? rtc.now() : startTime;
+DateTime RTCManager::now() {                            ///  
+  return running ? rtc.now() : startTime;               ///   
 }
 
-unsigned long RTCManager::elapsMillis() {
-  return running ? millis() - startMillis : 0;
+unsigned long RTCManager::elapsMillis() {                ///
+  return running ? millis() - startMillis : 0;           /// 
 }
 
